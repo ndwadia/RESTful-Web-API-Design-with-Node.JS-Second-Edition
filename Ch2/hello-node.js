c2e27b632c0744be9ac7531f08f050139c1e9be5
Neville <ndwadia@gmail.com>
2017-10-12 16:20:27 +0530

    ok

--- Ch2/hello-node.js
+++ Ch2/hello-node.js
@@ -3,7 +3,7 @@ var port = 8180;
 
 function handle_request(request, response) {
 	response.writeHead(200, {
-		'Content-Type' : 'text/plain'
+		'Content-Type': 'text/plain'
 	});
 
 	response.end('Hello World. Are you restless to go restful?\n');
@@ -13,4 +13,3 @@ function handle_request(request, response) {
 http.createServer(handle_request).listen(port, '127.0.0.1');
 
 console.log('Started Node.js http server at http://127.0.0.1:' + port);
-
