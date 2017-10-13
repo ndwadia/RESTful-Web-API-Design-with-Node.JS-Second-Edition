35b62cf30c33c53b55788e5e5656e3cb7906da39
Neville <ndwadia@gmail.com>
2017-10-13 23:23:27 +0530

    ok

--- Ch4/sources/test/contact-model-test.js
+++ Ch4/sources/test/contact-model-test.js
@@ -2,20 +2,27 @@ var mongoose = require('mongoose');
 var should = require('should');
 var prepare = require('./prepare');
 
-mongoose.connect('mongodb://localhost/contacts-test');
-
+var uri = 'mongodb://admin:8rjgg3rplmhe9tu@cluster0-shard-00-00-5mlyc.mongodb.net:27017,cluster0-shard-00-01-5mlyc.mongodb.net:27017,cluster0-shard-00-02-5mlyc.mongodb.net:27017/contacts-test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin';
+mongoose.connect(uri, {
+  useMongoClient: true
+});
 
 var contactSchema = new mongoose.Schema({
-	primarycontactnumber: {type: String, index: {unique: true}},
-	firstname: String,
-	lastname: String,
-	title: String,
-	company: String,
-	jobtitle: String,	
-	othercontactnumbers: [String],
-	primaryemailaddress: String,
-	emailaddresses: [String],
-	groups: [String]
+  primarycontactnumber: {
+    type: String,
+    index: {
+      unique: true
+    }
+  },
+  firstname: String,
+  lastname: String,
+  title: String,
+  company: String,
+  jobtitle: String,
+  othercontactnumbers: [String],
+  primaryemailaddress: String,
+  emailaddresses: [String],
+  groups: [String]
 });
 
 var Contact = mongoose.model('Contact', contactSchema);
@@ -25,25 +32,25 @@ describe('Contact: models', function () {
 
   describe('#create()', function () {
     it('Should create a new Contact', function (done) {
-      
+
       var contactModel = {
-    		  "firstname":"John",
-    		  "lastname":"Douglas",
-    		  "title":"Mr.",
-    		  "company":"Dev Inc.",
-    		  "jobtitle":"Developer",
-    		  "primarycontactnumber":"+359777223345",
-    		  "primaryemailaddress":"john.douglas@xyz.com",
-    		  "groups":["Dev"],
-    		  "emailaddresses":["j.douglas@xyz.com"],
-    		  "othercontactnumbers":['+359777223346','+359777223347']
+        "firstname": "John",
+        "lastname": "Douglas",
+        "title": "Mr.",
+        "company": "Dev Inc.",
+        "jobtitle": "Developer",
+        "primarycontactnumber": "+359777223345",
+        "primaryemailaddress": "john.douglas@xyz.com",
+        "groups": ["Dev"],
+        "emailaddresses": ["j.douglas@xyz.com"],
+        "othercontactnumbers": ['+359777223346', '+359777223347']
       };
-      
+
       Contact.create(contactModel, function (err, createdModel) {
         // Check that no error occured
         should.not.exist(err);
         // Assert that the returned contact has  is what we expect
-        
+
         createdModel.firstname.should.equal('John');
         createdModel.lastname.should.equal('Douglas');
         createdModel.title.should.equal('Mr.');
@@ -62,4 +69,3 @@ describe('Contact: models', function () {
 
 
 });
-
