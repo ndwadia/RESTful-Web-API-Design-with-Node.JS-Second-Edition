4fba00fb9e1c0a308fd697d202b5847c25b49524
Neville <ndwadia@gmail.com>
2017-10-15 15:22:44 +0530

    ok

--- Ch6/OLD/source/modules/contactdataservice_v1.js
+++ Ch6/OLD/source/modules/contactdataservice_v1.js
@@ -1,72 +1,80 @@
 exports.remove = function (model, _primarycontactnumber, response) {
-	console.log('Deleting contact with primary number: ' 
-			+ _primarycontactnumber);	
-	model.findOne({primarycontactnumber: _primarycontactnumber}, function(error, data) {
+	console.log('Deleting contact with primary number: ' +
+		_primarycontactnumber);
+	model.findOne({
+		primarycontactnumber: _primarycontactnumber
+	}, function (error, data) {
 		if (error) {
 			console.log(error);
 			if (response != null) {
-				response.writeHead(500, {'Content-Type' : 'text/plain'});			
+				response.writeHead(500, {
+					'Content-Type': 'text/plain'
+				});
 				response.end('Internal server error');
 			}
 			return;
-		} else {	
+		} else {
 			if (!data) {
-				console.log('not found');				
-				if (response != null)
-				{
-					response.writeHead(404, {'Content-Type' : 'text/plain'});			
+				console.log('not found');
+				if (response != null) {
+					response.writeHead(404, {
+						'Content-Type': 'text/plain'
+					});
 					response.end('Not Found');
 				}
 				return;
-			} else {	
-				data.remove(function(error){
+			} else {
+				data.remove(function (error) {
 					if (!error) {
-						data.remove();						
-											
-					}
-					else {
+						data.remove();
+
+					} else {
 						console.log(error);
 					}
-				});				
-				
-				if (response != null){
-					response.send('Deleted');	
-				}				
+				});
+
+				if (response != null) {
+					response.send('Deleted');
+				}
 				return;
 			}
-		}		
-	});	
+		}
+	});
 }
 
 exports.update = function (model, requestBody, response) {
-	
+
 	var primarynumber = requestBody.primarycontactnumber;
-	model.findOne({primarycontactnumber: primarynumber}, function(error, data) {
+	model.findOne({
+		primarycontactnumber: primarynumber
+	}, function (error, data) {
 		if (error) {
 			console.log(error);
 			if (response != null) {
-				response.writeHead(500, {'Content-Type' : 'text/plain'});			
+				response.writeHead(500, {
+					'Content-Type': 'text/plain'
+				});
 				response.end('Internal server error');
 			}
 			return;
 		} else {
 			var contact = toContact(requestBody, model);
 			if (!data) {
-				console.log('Contact with primary number: ' + primarynumber + 
-						' does not exist. The contact will be created.');				
-				
-				contact.save(function(error) {
+				console.log('Contact with primary number: ' + primarynumber +
+					' does not exist. The contact will be created.');
+
+				contact.save(function (error) {
 					if (!error)
-					contact.save();
+						contact.save();
 				});
-				
+
 				if (response != null) {
 					response.end('Created');
 				}
 				return;
 			}
 			//poulate the document with the updated values
-			
+
 			data.firstname = contact.firstname;
 			data.lastname = contact.lastname;
 			data.title = contact.title;
@@ -77,10 +85,10 @@ exports.update = function (model, requestBody, response) {
 			data.emailaddresses = contact.emailaddresses;
 			data.primaryemailaddress = contact.primaryemailaddress;
 			data.groups = contact.groups;
-			
+
 			data.save(function (error) {
 				if (!error) {
-					console.log('Successfully updated contact with primary number: '+ primarynumber);
+					console.log('Successfully updated contact with primary number: ' + primarynumber);
 					data.save();
 				} else {
 					console.log('error on save');
@@ -96,38 +104,44 @@ exports.update = function (model, requestBody, response) {
 exports.create = function (model, requestBody, response) {
 	var contact = toContact(requestBody, model);
 	var primarynumber = requestBody.primarycontactnumber;
-	contact.save(function(error) {
-		
-		if (!error) {			
-				contact.save();				
-		} else {			
+	contact.save(function (error) {
+
+		if (!error) {
+			contact.save();
+		} else {
 			console.log('Checking if contact saving failed due to already existing primary number:' + primarynumber);
-			model.findOne({primarycontactnumber: primarynumber}, function(error, data) {
+			model.findOne({
+				primarycontactnumber: primarynumber
+			}, function (error, data) {
 				if (error) {
 					console.log(error);
-					if (response != null) {						
-						response.writeHead(500, {'Content-Type' : 'text/plain'});			
+					if (response != null) {
+						response.writeHead(500, {
+							'Content-Type': 'text/plain'
+						});
 						response.end('Internal server error');
-					}					
+					}
 					return;
-				} else {					
+				} else {
 					var contact = toContact(requestBody, model);
-					if (!data) {						
-						console.log('The contact does not exist. It will be created');						
-						contact.save(function(error) {
+					if (!data) {
+						console.log('The contact does not exist. It will be created');
+						contact.save(function (error) {
 							if (!error) {
 								contact.save();
 							} else {
 								console.log(error);
-							}							
+							}
 						});
-						
+
 						if (response != null) {
-							response.writeHead(201, {'Content-Type' : 'text/plain'});
+							response.writeHead(201, {
+								'Content-Type': 'text/plain'
+							});
 							response.end('Created');
 						}
 						return;
-					} else {					
+					} else {
 						console.log('Updating contact with primary contact number:' + primarynumber);
 						data.firstname = contact.firstname;
 						data.lastname = contact.lastname;
@@ -139,9 +153,9 @@ exports.create = function (model, requestBody, response) {
 						data.emailaddresses = contact.emailaddresses;
 						data.primaryemailaddress = contact.primaryemailaddress;
 						data.groups = contact.groups;
-						
+
 						data.save(function (error) {
-							if (!error) {							
+							if (!error) {
 								data.save();
 								response.end('Updated');
 								console.log('Successfully Updated contat with primary contact number: ' + primarynumber);
@@ -152,63 +166,68 @@ exports.create = function (model, requestBody, response) {
 						});
 					}
 				}
-			});	
+			});
 		}
 	});
 };
 
 exports.findByNumber = function (model, _primarycontactnumber, response) {
-	
-	model.findOne({primarycontactnumber: _primarycontactnumber}, function(error, result) {
-		if (error) {			
+
+	model.findOne({
+		primarycontactnumber: _primarycontactnumber
+	}, function (error, result) {
+		if (error) {
 			console.error(error);
-			response.writeHead(500, {'Content-Type' : 'text/plain'});			
+			response.writeHead(500, {
+				'Content-Type': 'text/plain'
+			});
 			response.end('Internal server error');
 			return;
 		} else {
 			if (!result) {
 				if (response != null) {
-					response.writeHead(404, {'Content-Type' : 'text/plain'});			
-					response.end('Not Found');					
+					response.writeHead(404, {
+						'Content-Type': 'text/plain'
+					});
+					response.end('Not Found');
 				}
 				return;
 			}
-				
-			if (response != null){
+
+			if (response != null) {
 				response.setHeader('Content-Type', 'application/json');
-				response.send(result);				
-			}						
+				response.send(result);
+			}
 		}
-	});	
+	});
 };
 
 exports.list = function (model, response) {
-	model.find({}, function(error, result) {
+	model.find({}, function (error, result) {
 		if (error) {
 			console.error(error);
-			return null;	
+			return null;
 		}
 		if (response != null) {
-			response.setHeader('content-type', 'application/json');			
+			response.setHeader('content-type', 'application/json');
 			response.end(JSON.stringify(result));
 		}
 		return JSON.stringify(result);
 	});
 }
 
-function toContact(body, Contact) {	
-	
-	return new Contact(
-			{
-				firstname: body.firstname,
-				lastname: body.lastname,
-				title: body.title,
-				company: body.company,
-				jobtitle: body.jobtitle,
-				primarycontactnumber: body.primarycontactnumber,
-				primaryemailaddress: body.primaryemailaddress,				
-				emailaddresses: body.emailaddresses,
-				groups: body.groups,
-				othercontactnumbers: body.othercontactnumbers				
-			});
+function toContact(body, Contact) {
+
+	return new Contact({
+		firstname: body.firstname,
+		lastname: body.lastname,
+		title: body.title,
+		company: body.company,
+		jobtitle: body.jobtitle,
+		primarycontactnumber: body.primarycontactnumber,
+		primaryemailaddress: body.primaryemailaddress,
+		emailaddresses: body.emailaddresses,
+		groups: body.groups,
+		othercontactnumbers: body.othercontactnumbers
+	});
 }
--- Ch6/source/modules/admin.js
+++ Ch6/source/modules/admin.js
@@ -1,67 +1,77 @@
 exports.remove = function (model, _username, response) {
-	console.log('Deleting user: '+ _username);
-	
-	model.findOne({username: _username}, function(error, data) {
+	console.log('Deleting user: ' + _username);
+
+	model.findOne({
+		username: _username
+	}, function (error, data) {
 		if (error) {
 			console.log(error);
 			if (response != null) {
-				response.writeHead(500, {'Content-Type' : 'text/plain'});			
+				response.writeHead(500, {
+					'Content-Type': 'text/plain'
+				});
 				response.end('Internal server error');
 			}
 			return;
-		} else {	
+		} else {
 			if (!data) {
-				console.log('User' + _username + ' not found');				
-				if (response != null)
-				{
-					response.writeHead(404, {'Content-Type' : 'text/plain'});			
+				console.log('User' + _username + ' not found');
+				if (response != null) {
+					response.writeHead(404, {
+						'Content-Type': 'text/plain'
+					});
 					response.end('Not Found');
 				}
 				return;
-			} else {	
-				data.remove(function(error){
+			} else {
+				data.remove(function (error) {
 					if (!error) {
-						data.remove();				
-					}
-					else {
+						data.remove();
+					} else {
 						console.log(error);
 					}
-				});				
-				
-				if (response != null){
-					response.send('Deleted');	
-				}				
+				});
+
+				if (response != null) {
+					response.send('Deleted');
+				}
 				return;
 			}
-		}		
-	});	
+		}
+	});
 }
 
 exports.update = function (model, requestBody, response) {
-	
+
 	var _username = requestBody.username;
-	console.log (requestBody);
-	model.findOne({username: _username}, function(error, data) {
+	console.log(requestBody);
+	model.findOne({
+		username: _username
+	}, function (error, data) {
 		if (error) {
 			console.log(error);
 			if (response != null) {
-				response.writeHead(500, {'Content-Type' : 'text/plain'});			
+				response.writeHead(500, {
+					'Content-Type': 'text/plain'
+				});
 				response.end('Internal server error');
 			}
 			return;
 		} else {
 			var user = toAuthUser(requestBody, model);
 			if (!data) {
-				console.log('User: ' + _username + 
-						' does not exist. It will be created.');				
-				
-				user.save(function(error) {
+				console.log('User: ' + _username +
+					' does not exist. It will be created.');
+
+				user.save(function (error) {
 					if (!error)
-					user.save();
+						user.save();
 				});
-				
+
 				if (response != null) {
-					response.writeHead(201, {'Content-Type' : 'text/plain'});
+					response.writeHead(201, {
+						'Content-Type': 'text/plain'
+					});
 					response.end('Created');
 				}
 				return;
@@ -69,11 +79,11 @@ exports.update = function (model, requestBody, response) {
 
 			data.username = user.username;
 			data.password = user.password;
-			data.role = user.role;			
-			
+			data.role = user.role;
+
 			data.save(function (error) {
 				if (!error) {
-					console.log('Successfully updated user: '+ _username);
+					console.log('Successfully updated user: ' + _username);
 					data.save();
 				} else {
 					console.log('Error on save operation');
@@ -89,46 +99,52 @@ exports.update = function (model, requestBody, response) {
 exports.create = function (model, requestBody, response) {
 	var user = toAuthUser(requestBody, model);
 	var _username = requestBody.username;
-	user.save(function(error) {
-		
-		if (!error) {			
-			user.save();				
-		} else {			
+	user.save(function (error) {
+
+		if (!error) {
+			user.save();
+		} else {
 			console.log('Checking if user saving failed due to already existing user:' + _username);
-			model.findOne({username: _username}, function(error, data) {
+			model.findOne({
+				username: _username
+			}, function (error, data) {
 				if (error) {
 					console.log(error);
-					if (response != null) {						
-						response.writeHead(500, {'Content-Type' : 'text/plain'});			
+					if (response != null) {
+						response.writeHead(500, {
+							'Content-Type': 'text/plain'
+						});
 						response.end('Internal server error');
-					}					
+					}
 					return;
-				} else {					
+				} else {
 					var user = toAuthUser(requestBody, model);
-					if (!data) {						
-						console.log('The user does not exist. It will be created');						
-						user.save(function(error) {
+					if (!data) {
+						console.log('The user does not exist. It will be created');
+						user.save(function (error) {
 							if (!error) {
 								user.save();
 							} else {
 								console.log(error);
-							}							
+							}
 						});
-						
+
 						if (response != null) {
-							response.writeHead(201, {'Content-Type' : 'text/plain'});
+							response.writeHead(201, {
+								'Content-Type': 'text/plain'
+							});
 							response.end('Created');
 						}
 						return;
-					} else {					
+					} else {
 						console.log('Updating user:' + _username);
-						
+
 						data.username = user.username;
 						data.password = user.password;
 						data.role = user.role;
-											
+
 						data.save(function (error) {
-							if (!error) {							
+							if (!error) {
 								data.save();
 								response.end('Updated');
 								console.log('Successfully updated user: ' + _username);
@@ -139,18 +155,17 @@ exports.create = function (model, requestBody, response) {
 						});
 					}
 				}
-			});	
+			});
 		}
 	});
 };
 
 
-function toAuthUser(body, AuthUser) {	
-	
-	return new AuthUser(
-			{
-				username: body.username,
-				password: body.password,
-				role: body.role
-			});
+function toAuthUser(body, AuthUser) {
+
+	return new AuthUser({
+		username: body.username,
+		password: body.password,
+		role: body.role
+	});
 }
